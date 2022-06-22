b787ccde682f3bd5cabdd4c367b2cc2566e3463e
Falak Grover <falakgrover09@gmail.com>
2022-06-22 14:09:54 -0500

    Initial Commit

---
+++ ActionsOutlets22kwk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ActionsOutlets22kwk
+//
+//  Created by Scholar on 6/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
